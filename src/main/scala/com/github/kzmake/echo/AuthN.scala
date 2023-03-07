e62af8c0be96db136287be0cb3b35561236efcaa
kzmake <kzmake.i3a@gmail.com>
2023-03-07 21:23:21 +0900

    authn user

---
+++ src/main/scala/com/github/kzmake/echo/AuthN.scala
@@ -0,0 +1,11 @@
+package com.github.kzmake.echo
+
+import org.atnos.eff.Eff
+import org.atnos.eff.syntax.all._
+import zio.http.Request
+
+trait AuthN {
+  type AuthenticatedUser = String
+
+  def authenticate[R](req: Request): Eff[R, AuthenticatedUser] = "alice".pureEff[R]
+}
