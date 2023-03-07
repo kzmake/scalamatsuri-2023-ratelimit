364e820278c6646f0f618441e2ee9fd628777d4c
kzmake <kzmake.i3a@gmail.com>
2023-03-07 21:23:36 +0900

    add TextResponse

---
+++ src/main/scala/com/github/kzmake/echo/TextResponse.scala
@@ -0,0 +1,23 @@
+package com.github.kzmake.echo
+
+import com.github.kzmake.error.TooManyRequestError
+import zio.http._
+import zio.http.model._
+
+trait TextResponse {
+  def toResponse(v: Either[Throwable, Response]): Response = v match {
+    case Right(res) =>
+      res
+
+    case Left(TooManyRequestError(message, retryAfter)) =>
+      Response(
+        status = Status.TooManyRequests,
+        body = Body.fromString(message),
+      ).withRetryAfter(retryAfter.toString)
+    case Left(err) =>
+      Response(
+        status = Status.InternalServerError,
+        body = Body.fromString(err.getMessage),
+      )
+  }
+}
