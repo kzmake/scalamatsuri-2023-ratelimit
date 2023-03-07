502e0200460dd3c7c180548c016ef1c29931e6ae
kzmake <kzmake.i3a@gmail.com>
2023-03-07 21:44:44 +0900

    remove unused deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,10 @@
 import sbt._
 
 object Dependencies {
-  val ZIOVersion   = "2.0.9"
-  val ZHTTPVersion = "0.0.4"
-
+  val ZHTTPVersion    = "0.0.4"
   val `zio-http`      = "dev.zio" %% "zio-http"         % ZHTTPVersion
   val `zio-http-test` = "dev.zio" %% "zio-http-testkit" % ZHTTPVersion % Test
 
-  val zio            = "dev.zio" %% "zio"          % ZIOVersion
-  val `zio-test`     = "dev.zio" %% "zio-test"     % ZIOVersion % Test
-  val `zio-test-sbt` = "dev.zio" %% "zio-test-sbt" % ZIOVersion % Test
-
-  val AtnosEff = "6.0.2"
-
+  val AtnosEff    = "6.0.2"
   val `atnos-eff` = "org.atnos" %% "eff" % AtnosEff
 }
